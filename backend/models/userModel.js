06ad83f0a42f04e96f18bc04227e595da28d52a7
lisakikuch <lisathichanpho@gmail.com>
2025-03-30 21:51:17 -0400

    Fixed a typo in userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,7 +15,7 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        require: [true, "Password is required"],
+        required: [true, "Password is required"],
         minlength: [6, "Password must be at least 6 characters long"],
     },
     role: {
