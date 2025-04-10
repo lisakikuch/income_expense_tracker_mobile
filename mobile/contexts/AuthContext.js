de94837f2591d9d348dea38cec9fae5a61070843
lisakikuch <lisathichanpho@gmail.com>
2025-04-10 10:55:34 -0400

    Updated Packages

--- mobile/contexts/AuthContext.js
+++ mobile/contexts/AuthContext.js
@@ -1,10 +1,15 @@
+// React Hooks used to create & manage authentication state
 import React, { createContext, useEffect, useState } from "react";
 import axios from 'axios';
+// A secure storage API from Expo to persist sensitive info like JWT tokens.
 import * as SecureStore from 'expo-secure-store';
 import { API_URL } from '@env';
 
+// Create a new context object, allowing us to share
+// authentication-related data & functions throughout the component
 const AuthContext = createContext();
 
+// Wrap the app & provide authentication data to all nested components via context
 export const AuthProvider = ({ children }) => {
     // Define variables holding a user object and its jwt received from backend
     const [user, setUser] = useState(null);
